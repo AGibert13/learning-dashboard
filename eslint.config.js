50c02a322517ea0bf99a1772e6f773ac17305b16
AGibert13 <adriengibert13@gmail.com>
2025-12-27 17:48:51 -0500

    chore: improve documentation and lint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,10 @@ const globals = require("globals");
 const pluginReact = require("eslint-plugin-react");
 const { defineConfig } = require("eslint/config");
 const jest = require('eslint-plugin-jest');
+const { includeIgnoreFile } = require('@eslint/compat')
+const path = require("node:path");
+
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 
 module.exports = defineConfig([
@@ -24,12 +28,27 @@ module.exports = defineConfig([
     }
   },
   {
-    files: ['test/**'],
+    files: ['tests/**/*.js'],
     ...jest.configs['flat/recommended'],
     rules: {
       ...jest.configs['flat/recommended'].rules,
       'jest/prefer-expect-assertions': 'off',
     },
   },
+  {
+    files: ["src/**/*.js"],
+    rules: {
+      //TODO V2: Replace with proper logging Library
+      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+      'consistent-return': 'error',
+      'no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }],
+      'require-await': 'error',
+      'no-floating-promises': 'off'
+    }
+  },
+  includeIgnoreFile(gitignorePath),
   pluginReact.configs.flat.recommended,
 ]);
